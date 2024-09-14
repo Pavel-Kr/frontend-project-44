ee088701c40236cbadad357c188517f7d517a710
Pavel Krivosheev <pkrivosheev@bk.ru>
2024-09-14 22:40:40 +0700

    Smol refactoring

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,10 +1,4 @@
 #!/usr/bin/env node
-import getName from '../src/cli.js';
+import greetUser from '../src/cli.js';
 
-const main = () => {
-  console.log('Welcome to the Brain Games!');
-  const name = getName();
-  console.log(`Hello, ${name}!`);
-};
-
-main();
+greetUser();
